16825fa8da19aaafe0f50ca95a5d4b3291e8e435
Josh Shone <josh.shone@kiwi.ki>
2018-01-14 22:44:58 +0100

    Tweak handling of html script tag compilation

--- html_compiler.js
+++ html_compiler.js
@@ -2,19 +2,25 @@ function compileHtml(node) {
   return followListNodes(node, 'next').map(currentNode => {
     var tag = findNodeVia(currentNode, 'tag');
     if (tag) {
-      var child = findNodeVia(currentNode, 'child');
-      if (child) {
-        return '<' + tag.value + '>' + compileHtml(child) + '</' + tag.value + '>';
-      }
-      var content = findNodeVia(currentNode, 'text');
-      if (content) {
-        if (isJs(content)) {
-          return '<' + tag.value + '>' + compileStatements(content) + '</' + tag.value + '>';
+      if (tag.value === 'script') {
+        var text = findNodeVia(currentNode, 'text');
+        if (text) {
+          return '<script>' + compileStatements(text) + '</script>';
         } else {
-          return '<' + tag.value + '>' + content.value + '</' + tag.value + '>';
+          return '<script></script>';
+        }
+      } else {
+        var child = findNodeVia(currentNode, 'child');
+        if (child) {
+          return '<' + tag.value + '>' + compileHtml(child) + '</' + tag.value + '>';
+        }
+        var text = findNodeVia(currentNode, 'text');
+        if (text) {
+          return '<' + tag.value + '>' + text.value + '</' + tag.value + '>';
+        } else {
+          return '<' + tag.value + '></' + tag.value + '>';
         }
       }
-      return '<' + tag.value + '></' + tag.value + '>';
     }
   }).join('');
 }
