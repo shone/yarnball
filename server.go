73314d04dc91bb3cbfa95ed01182de446be4b11b
Josh Shone <josh.shone@gmail.com>
2020-07-19 21:05:30 +0200

    Apply gofmt to server.go, split out http handlers and use http.Error

--- server.go
+++ server.go
@@ -1,75 +1,76 @@
 package main
+
 import (
-  "net/http"
-  "io/ioutil"
-  "log"
-  "os"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"errors"
 )
 
-func NoCache(handler http.Handler) http.Handler {
-  return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-    writer.Header().Set("Cache-Control", "must-revalidate")
-    handler.ServeHTTP(writer, request)
-  })
+func main() {
+	http.Handle("/", NoCache(http.FileServer(http.Dir("."))))
+
+	http.HandleFunc("/load", handleLoad)
+	http.HandleFunc("/save", handleSave)
+
+	err := http.ListenAndServe(":8089", nil)
+	panic(err)
 }
 
-func main() {
-  http.Handle("/", NoCache(http.FileServer(http.Dir("."))))
+func NoCache(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Cache-Control", "must-revalidate")
+		handler.ServeHTTP(writer, request)
+	})
+}
 
-  http.HandleFunc("/load", func(response http.ResponseWriter, request *http.Request) {
-    if request.Method != "GET" {
-      response.WriteHeader(http.StatusBadRequest)
-      response.Write([]byte("Method not supported"))
-      return
-    }
-    paths,hasPath := request.URL.Query()["path"]
-    if !hasPath {
-      response.WriteHeader(http.StatusBadRequest)
-      response.Write([]byte("Missing path query"))
-      return
-    }
-    path := paths[0]
-    content,err := ioutil.ReadFile(path)
-    if err != nil {
-      pathError,isPathError := err.(*os.PathError)
-      if isPathError {
-        if pathError.Err.Error() == "no such file or directory" {
-          response.WriteHeader(http.StatusNotFound)
-          response.Write([]byte(pathError.Err.Error()))
-          return
-        }
-      }
-      response.WriteHeader(http.StatusInternalServerError)
-      return
-    }
-    response.Write(content)
-  })
+func handleLoad(response http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		http.Error(response, "Method not supported", http.StatusBadRequest)
+		return
+	}
 
-  http.HandleFunc("/save", func(response http.ResponseWriter, request *http.Request) {
-    if request.Method != "PUT" {
-      response.WriteHeader(http.StatusBadRequest)
-      response.Write([]byte("Method not supported"))
-      return
-    }
-    paths,hasPath := request.URL.Query()["path"]
-    if !hasPath {
-      response.WriteHeader(http.StatusBadRequest)
-      response.Write([]byte("Missing path query"))
-      return
-    }
-    path := paths[0]
-    defer request.Body.Close()
-    body,err := ioutil.ReadAll(request.Body)
-    if err != nil {
-      response.WriteHeader(http.StatusInternalServerError)
-      response.Write([]byte("Could not read request body"))
-      return
-    }
-    ioutil.WriteFile(path, body, 0644)
-  })
+	paths, hasPath := request.URL.Query()["path"]
+	if !hasPath {
+		http.Error(response, "Missing path query", http.StatusBadRequest)
+		return
+	}
 
-  log.Println("Serving on port 8089..")
-  if err := http.ListenAndServe(":8089", nil); err != nil {
-    panic(err)
-  }
+	path := paths[0]
+
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			http.Error(response, err.Error(), http.StatusNotFound)
+		} else {
+			http.Error(response, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	response.Write(content)
+}
+
+func handleSave(response http.ResponseWriter, request *http.Request) {
+	if request.Method != "PUT" {
+		http.Error(response, "Method not supported", http.StatusBadRequest)
+		return
+	}
+
+	paths, hasPath := request.URL.Query()["path"]
+	if !hasPath {
+		http.Error(response, "Missing path query", http.StatusBadRequest)
+		return
+	}
+
+	path := paths[0]
+
+	body, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(response, "Could not read request body", http.StatusInternalServerError)
+		return
+	}
+	defer request.Body.Close()
+
+	ioutil.WriteFile(path, body, 0644)
 }
